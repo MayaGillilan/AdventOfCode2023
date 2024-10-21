880340b2cb307091b28fa046874518298e2def1f
MayaGillilan <101652314+MayaGillilan@users.noreply.github.com>
2024-10-21 16:21:07 +0200

    day1: solve part 2

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
 
 /*
   Solves trebuchet problem https://adventofcode.com/2023/day/1 - Given a file with each line containing a jumbled string,
   find the first and last digit of each line and add them together to form a number. Sum the number from each line
   and print the total sum.
+
+  Part 2: Digits can also be spelled out, like 'one' or 'four'. This should also be accepted.
 */
 
 int main()
@@ -26,12 +29,33 @@ int main()
 
   std::string currentLine{};
   int sum{};
+  const std::map<std::string, char> digitWordMap{
+      {"one", '1'}, {"two", '2'}, {"three", '3'}, {"four", '4'}, {"five", '5'}, {"six", '6'}, {"seven", '7'}, {"eight", '8'}, {"nine", '9'}};
 
   while (std::getline(inputFile, currentLine))
   {
     size_t firstDigitPos{currentLine.find_first_of("0123456789")};
     size_t secondDigitPos{currentLine.find_last_of("0123456789")};
-    sum += std::stoi(std::string() + currentLine[firstDigitPos] + currentLine[secondDigitPos], nullptr, 10);
+    char firstDigit{currentLine[firstDigitPos]};
+    char secondDigit{currentLine[secondDigitPos]};
+
+    for (const auto &[key, value] : digitWordMap)
+    {
+      const int firstMatchPos = currentLine.find(key);
+      if (firstMatchPos != -1 && firstMatchPos < firstDigitPos)
+      {
+        firstDigit = value;
+        firstDigitPos = firstMatchPos;
+      }
+      const int lastMatchPos = currentLine.rfind(key);
+      if (lastMatchPos != -1 && lastMatchPos > secondDigitPos)
+      {
+        secondDigit = value;
+        secondDigitPos = lastMatchPos;
+      }
+    }
+
+    sum += std::stoi(std::string() + firstDigit + secondDigit, nullptr, 10);
   }
 
   std::cout << "Sum of each line's first and last digit is: " << sum << '\n';
